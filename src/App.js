3552ac9e631c01edb0195730debb38359799391d
tbbcoach <58452640+tbbcoach@users.noreply.github.com>
2021-01-07 20:02:52 -0500

    created product context.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import data from './data';
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
+import { ProductContext } from './contexts/ProductContext';
 
 function App() {
 	const [products] = useState(data);
@@ -17,16 +18,18 @@ function App() {
 
 	return (
 		<div className="App">
+			<ProductContext.Provider value = {{ products, addItem }}>
 			<Navigation cart={cart} />
 
 			{/* Routes */}
 			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
+				<Products />
 			</Route>
 
 			<Route path="/cart">
 				<ShoppingCart cart={cart} />
-			</Route>
+				</Route>
+			</ProductContext.Provider>
 		</div>
 	);
 }
